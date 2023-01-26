70b3ba1c030d5b3debc4910ad5dea9ffe350d9a3
juan barrios <juanpablobarriosgrijalba@gmail.com>
2023-01-26 17:16:26 -0500

    Seccion finalizada HeroApp

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -13,6 +13,10 @@ const { q =" "} = queryString.parse(location.search);
 
 const heroes = getHeroByname(q);
 
+const showSearch = (q.length === 0) ;
+const showError = (q.length > 0) && heroes.length === 0;
+
+
  const {searchText,onInputChange} = useForm({
 
 searchText: q 
@@ -21,7 +25,7 @@ searchText: q
 
 const onsSearchSubmit = (event)=>{
 event.preventDefault();
-if(searchText.trim().length <=1 )return;
+// if(searchText.trim().length <=1 )return;
 
 navigate (`?q=${searchText}`);
 
@@ -54,8 +58,19 @@ navigate (`?q=${searchText}`);
         <div className="col-7">
           <h4> Result</h4>
           <hr />
-          <div className="alert alert-primary">Search a hero</div>
-          <div className="alert alert-danger">No hero with <b>{q}</b></div>
+
+        
+  
+          <div className="alert alert-primary animate__animated animate__fadeIn "
+           style={{ display: showSearch ? '' : 'none'}}
+            >Search a hero
+          </div>
+
+          <div className="alert alert-danger animate__animated animate__fadeIn"
+           style={{ display: showError ? '' : 'none'}}>
+            No hero with <b>{q}</b>
+         </div>
+
           {
             heroes.map( hero =>(
               <HeroCard key={hero.id} {...hero}/> 
